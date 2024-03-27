4282a809414f96cc0d41e8a38abac2c3d0fcdbbb
Digvijay Singh <dvsingh958@gmail.com>
2024-03-27 18:07:49 +0530

    day 5 registration route complete

--- project setup/src/controllers/user.controller.js
+++ project setup/src/controllers/user.controller.js
@@ -16,14 +16,23 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required!");
   } //validation for username, fullname, email, password
 
-  const exisitingUser = User.findOne({
+  const exisitingUser = await User.findOne({
     $or: [{ userName }, { email }],
   }); //validation for existing user
 
   if (exisitingUser) throw new ApiError(409, "User already exists.");
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files?.coverImage[0]?.path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar image is required!");
